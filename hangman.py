32e82870ad05b1f2f87f9cb9bfe4c590094fbba9
nolanium <peterl.solymosi@gmail.com>
2021-09-21 14:09:13 +0200

    hangman.py

--- hangman.py
+++ hangman.py
@@ -1,5 +1,22 @@
-def play(word, lives):
-    pass
+import random
+print ("Welcome to HANG == MAN")
 
+levels = 0
+lives = 0
 
-play('Codecool', 6)
+while True:
+    x = input ("Pls choose your level: 1 for rookie or 2 for expert. \n")
+    if x == ("1"):
+        levels = 1
+        lives = 6
+        print ("You choose the rookie level.")
+        break
+    
+    if x == ("2"):
+        levels = 2
+        lives = 3
+        print ("You choose the expert level.")
+        break
+
+words = open("countries-and-capitals.txt")
+hangman_list = []
