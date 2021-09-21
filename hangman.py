90bb9e4864779092f341e9b0d85225bbf02ee942
nolanium <peterl.solymosi@gmail.com>
2021-09-21 14:49:47 +0200

    hangman.py

--- hangman.py
+++ hangman.py
@@ -5,17 +5,17 @@ levels = 0
 lives = 0
 
 while True:
-    x = input ("Pls choose your level: 1 for rookie or 2 for expert. \n")
+    x = input ("Pls choose your level: 1 for rookie(6 lives) or 2 for expert(3 lives). \n")
     if x == ("1"):
         levels = 1
         lives = 6
-        print ("You choose the rookie level!")
+        print ("You choose the rookie level, with 6 lives!")
         break
     
     if x == ("2"):
         levels = 2
         lives = 3
-        print ("You choose the expert level!")
+        print ("You choose the expert level, with 3 lives!")
         break
     else:
         print("Pls choose a valid operator!")
