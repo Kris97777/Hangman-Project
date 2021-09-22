4ca8c55bea0a1dcfcf6b0d97182c67fc7f3db08b
nolanium <peterl.solymosi@gmail.com>
2021-09-22 11:58:09 +0200

    hangman.py

--- hangman.py
+++ hangman.py
@@ -17,9 +17,75 @@ while True:
         lives = 3
         print ("You choose the expert level, with 3 lives!")
         break
+    if x == ("quit"):
+        print("Good Bye!")
+        break
     else:
         print("Pls choose a valid operator!")
 
-with open("countries-and-capitals.txt") as f:
-    word_list = f.read().splitlines()
+def pick_a_random():
+    with open("countries.txt") as f:
+        word_list = []
 
+    print(random.choice(open('countries.txt').read().split()).strip()) #by anandskumar, thx :)
+
+
+
+while True:
+    if ((level == 2) and (lives == 3)) or ((level == 1) and ((lives ==6) or (lives == 5))):
+	    print("_________")
+	    print("|	 |  ")
+	    print("|        ")
+	    print("|        ")
+	    print("|        ")
+	    print("|        ")
+	    print("|________")
+    elif ((level == 2) and (lives == 2)) or ((level == 1) and ((lives == 4) or (lives == 3))):
+	    print ("_________")
+	    print ("|	 |")
+	    print ("|	 O")
+	    print ("|")
+	    print ("|")
+	    print ("|")
+	    print ("|________")
+    elif ((level == 2) and (lives == 1)) or ((level == 1) and ((lives ==2) or (lives == 1))):
+	    print ("_________")
+	    print ("|	 |")
+	    print ("|	 O")
+	    print ("|	 |")
+	    print ("|	 |")
+	    print ("|")
+	    print ("|________")
+    elif ((level == 2) and (lives == 3)) or ((level == 1) and ((lives == 6) or (lives == 5))):
+	    print ("_________")
+	    print ("|	 |")
+	    print ("|	 O")
+	    print ("|	\|")
+	    print ("|	 |")
+	    print ("|        ")
+	    print ("|________")
+    elif ((level == 2) and (lives == 2)) or ((level == 1) and ((lives ==4) or (lives == 3))):
+	    print ("_________")
+	    print ("|	 |")
+	    print ("|	 O")
+	    print ("|	\|/")
+	    print ("|	 |")
+	    print ("|")
+	    print ("|________")
+    elif ((level == 2) and (lives == 1)) or ((level == 1) and (lives == 2)):
+	    print ("_________")
+	    print ("|	 |")
+	    print ("|	 O")
+	    print ("|	\|/")
+	    print ("|	 |")
+	    print ("|	/ ")
+	    print ("|________")
+    elif (lives == 0):
+	    print ("_________")
+	    print ("|	 |")
+	    print ("|	 O")
+	    print ("|	\|/")
+	    print ("|	 |")
+	    print ("|	/ \ ")
+	    print ("|________")
+    print(string_game_word)
